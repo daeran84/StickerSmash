43193ac371a1d41c1f76be1bbeb526d38f25acbb
Camilo Rioseco Rodríguez <daeran84@gmail.com>
2025-01-05 19:26:09 -0500

    Status bar and Splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,16 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { LogBox } from "react-native";
 
-LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
     <>
-      <StatusBar style="light"/>
       <Stack>
-        <Stack.Screen 
-          name="(tabs)"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen name="+not-found"
-        options={{
-        }}/>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
       </Stack>
+      <StatusBar style="light" />
     </>
-  );  
+  );
 }
